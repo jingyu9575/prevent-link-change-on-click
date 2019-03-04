4bc688350a34e6366c4d8d57fe447558916b2ce2
jingyu9575 <qw2g64@outlook.com>
2019-03-04 19:58:22 +0800

    Change the condition for valid events

--- content.js
+++ content.js
@@ -2,7 +2,7 @@ window.eval('void (' + function () {
 	const desc = Object.getOwnPropertyDescriptor(HTMLAnchorElement.prototype, 'href')
 	Object.defineProperty(HTMLAnchorElement.prototype, 'href', Object.assign({}, desc, {
 		set: function (value) {
-			if (window.event instanceof MouseEvent) {
+			if (window.event && window.event.constructor.name === 'MouseEvent') {
 				const { type } = window.event
 				if (type === 'mousedown' || type === 'mouseup' || type === 'click') return
 			}
